50ebf481eb408944aa35c9f260d905b6bae75b70
JoeSpencer1 <108445563+JoeSpencer1@users.noreply.github.com>
2024-01-05 12:17:15 -0700

    Working on adding MFNN functionality.

--- src/nn.py
+++ src/nn.py
@@ -141,6 +141,133 @@ def validation_one(yname, trnames, tstname, type, train_size, lay=9, wid=32, ang
     with open('Output.txt', 'a') as f:
         f.write('validation_one [' + ' '.join(map(str, trnames)) + '] ' +  tstname + ' ' + yname + ' ' + str(lay) + ' ' + str(wid) + ' [' + stsize + '] ' + str(np.mean(mape, axis=0)) + ' ' + str(np.std(mape, axis=0)) + '\n')
 
+def validation_two(yname, tsize_1, tsize_2, train1_name, train2_name, test_name):
+    data_train1 = FEMDataT('../data/TI33_' + train1_name + '.csv')
+    data_train2 = FEMDataT('../data/TI33_' + train2_name + '.csv')
+    data_test = ExpDataT('../data/TI33_' + test_name + '.csv')
+
+    ape = []
+    y = []
+    kf = ShuffleSplit(n_splits=10, train_size=tsize_1, random_state=0)
+    for train_index, _ in kf.split(data_train1.X):
+        print('\nIteration: {}'.format(len(ape)))
+        print(train_index)
+        data = dde.data.MfDataSet(
+            X_lo_train=data_train1.X,
+            X_hi_train=data_train2.X,
+            y_lo_train=data_train1.y,
+            y_hi_train=data_train2.y,
+            X_hi_test=data_test.X,
+            y_hi_test=data_test.y,
+            standardize=True
+        )
+        res = dde.utils.apply(mfnn, (data,))
+        ape.append(res[:2])
+        y.append(res[2])
+
+    num = 5
+    y_res = [res[0]]
+    y_data = np.vstack((data_train1.y, data_train2.y))
+    
+
+def validation_joe(yname, tsize_1, tsize_2, train1_name, train2_name, test_name):
+    
+    dataFEM = FEMData(yname, [70])
+    dataBerkovich = BerkovichData(yname)
+    data_train1 = ExpData('../data/' + train1_name + '.csv', yname)
+    data_train2 = ExpData('../data/' + train2_name + '.csv', yname)
+    data_test = ExpData('../data/' + test_name + '.csv', yname)
+    
+    ape = []
+    y = []
+    
+    kf = ShuffleSplit(n_splits=10, train_size=tsize_1, random_state=0)
+    
+    if tsize_1 > 0:
+        for train_index, _ in kf.split(data_train1.X):
+            print("\nIteration: {}".format(len(ape)))
+            print(train_index)
+            data = dde.data.MfDataSet(
+                X_lo_train=dataFEM.X,
+                X_hi_train=np.vstack((dataBerkovich.X, data_train1.X[train_index])),
+                y_lo_train=dataFEM.y,
+                y_hi_train=np.vstack((dataBerkovich.y, data_train1.y[train_index])),
+                X_hi_test=data_test.X,
+                y_hi_test=data_test.y,
+                standardize=True
+            )
+            res = dde.utils.apply(mfnn, (data,))
+            ape.append(res[:2])
+            y.append(res[2])
+    else:
+        for train_index in range(10):
+            print("\nIteration: {}".format(train_index))
+            print(train_index)
+            data = dde.data.MfDataSet(
+                X_lo_train=dataFEM.X,
+                X_hi_train=dataBerkovich.X,
+                y_lo_train=dataFEM.y,
+                y_hi_train=dataBerkovich.y,
+                X_hi_test=data_test.X,
+                y_hi_test=data_test.y,
+                standardize=True
+            )
+            res = dde.utils.apply(mfnn, (data,))
+            ape.append(res[:2])
+            y.append(res[2])
+ 
+    num = 5
+    y_res = [res[0]]
+    y_data = np.vstack((dataBerkovich.y, data_train1.y[train_index]))
+    X_data = np.vstack((dataBerkovich.X, data_train1.X[train_index]))
+    find_index = np.argmax(y_data > y_res)
+    X_res = X_data[find_index]
+    y_res = np.repeat([y_res], num, axis = 0)
+    X_res = np.repeat([X_res], num, axis = 0)
+
+    kf = ShuffleSplit(n_splits=10, train_size=tsize_2, random_state=0)
+
+    if tsize_2 > 0:
+        for train_index, _ in kf.split(data_train2.X):
+            print("\nIteration: {}".format(len(ape)))
+            print(train_index)
+            data = dde.data.MfDataSet(
+                X_lo_train=dataBerkovich.X,
+                X_hi_train=np.vstack((X_res, data_train2.X[train_index])),
+                y_lo_train=dataBerkovich.y,
+                y_hi_train=np.vstack((y_res, data_train2.y[train_index])),
+                X_hi_test=data_test.X,
+                y_hi_test=data_test.y,
+                standardize=True
+            )
+            res = dde.utils.apply(mfnn, (data,))
+            ape.append(res[:2])
+            y.append(res[2])
+    else:
+        y_res = np.vstack((y_res, y_res))
+        X_res = np.vstack((X_res, X_res))
+        print('dataBerkovich.X: ', dataBerkovich.X)
+        print('X_res: ', X_res)
+        print('dataBerkovich.y: ', dataBerkovich.y)
+        print('y_res: ', y_res)
+        print('data_test.X: ', data_test.X)
+        print('data_test.y: ', data_test.y)
+        for train_index in range(10):
+            print("\nIteration: {}".format(train_index))
+            print(train_index)
+            data = dde.data.MfDataSet(
+                X_lo_train=dataBerkovich.X,
+                X_hi_train=X_res,
+                y_lo_train=dataBerkovich.y,
+                y_hi_train=y_res,
+                X_hi_test=data_test.X,
+                y_hi_test=data_test.y,
+                standardize=True
+            )
+            res = dde.utils.apply(mfnn, (data,))
+            ape.append(res[:2])
+            y.append(res[2])
+
 def main(argument=None):
     if argument != None:
         exec(argument)
