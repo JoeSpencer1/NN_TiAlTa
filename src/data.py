97df0694f5190f26e290bd11f626e506e187432c
JoeSpencer1 <108445563+JoeSpencer1@users.noreply.github.com>
2024-01-09 14:06:00 -0700

    Got Berkovich model working, sort of. Stay tuned for 2D FEM model

--- src/data.py
+++ src/data.py
@@ -8,12 +8,14 @@ import pandas as pd
 class FEMData(object):
     def __init__(self, yname):
         self.yname = yname
-        self.angles = angles
+        #self.angles = angles
+        self.angles = ['../data/TI33_conical_30.csv', '../data/TI33_conical_45.csv', '../data/TI33_conical_60.csv']
+        print('Size: '+len(self.angles))
 
         self.X = None
         self.y = None
 
-        self.read_3angles()
+        self.read_angles()
         '''
         if len(angles) == 1:
             self.read_1angle()
@@ -22,13 +24,12 @@ class FEMData(object):
         elif len(angles) == 3:
             self.read_3angles()'''
 
-    def read_3angles(self):
+    def read_angles(self):
         df1 = pd.read_csv('../data/TI33_conical_30.csv')
         df2 = pd.read_csv('../data/TI33_conical_45.csv')
         df3 = pd.read_csv('../data/TI33_conical_60.csv')
         df = (
             df3.set_index('Case')
-            .join(df1.set_index('Case'), how='inner', rsuffix='_30')
             .join(df2.set_index('Case'), how='inner', rsuffix='_45')
             .join(df3.set_index('Case'), how='inner', rsuffix='_60')
         )
@@ -36,18 +37,63 @@ class FEMData(object):
 
         self.X = df[
             [
-                'C (GPa)_30',
-                'C (GPa)_45',
-                'C (GPa)_60',
+                'C (GPa)',
                 'dP/dh (N/m)',
-                'Wp/Wt',
+                'Wp/Wt'
             ]
         ].values
         if self.yname == 'Estar':
-            self.y = EtoEr(df['E (GPa)', 'nu'].values)[:, None]
+            self.y = EtoEr(df['E (GPa)'].values, df['nu'].values)[:, None]
         elif self.yname == 'sigma_y':
             self.y = df['sy (GPa)'].values[:, None]
 
+class ExpData(object):
+    def __init__(self, temp, yname):
+        filename = '../data/' + temp + '.csv'
+        self.filename = filename
+        self.yname = yname
+
+        self.X = None
+        self.y = None
+
+        self.read()
+
+    def read(self):
+        df = pd.read_csv(self.filename)
+
+        #
+        # Scale dP/dh from 3N to hm = 0.2um
+
+# This is for Al alloys
+#        df['dP/dh (N/m)'] *= 0.2 * (df['C (GPa)'] / 3) ** 0.5 * 10 ** (-1.5)
+
+        
+        # Scale dP/dh from Pm to hm = 0.2um
+        # df['dP/dh (N/m)'] *= 0.2 * (df['C (GPa)'] / df['Pm (N)']) ** 0.5 * 10 ** (-1.5)
+        # Scale dP/dh from hm to hm = 0.2um 
+
+# This is for Ti alloys
+#        df['dP/dh (N/m)'] *= 0.2 / df['hm (um)']
+# This is for the Yanbo's Ti alloys
+        df['dP/dh (N/m)'] *= 0.2 * 1000 / df['hmax(nm)']
+
+        # Scale c* from Berkovich to Conical
+#        df['dP/dh (N/m)'] *= 1.128 / 1.167
+        #
+
+        print(df.describe())
+
+# I just commented this line for my own work.
+        self.X = df[['C (GPa)', 'dP/dh (N/m)', 'Wp/Wt']].values
+        if self.yname == 'Estar':
+            self.y = df['Estar (GPa)'].values[:, None]
+        if self.yname.startswith('E_'):
+            self.y = df[self.yname].values[:, None]
+        elif self.yname == 'sigma_y':
+            self.y = df['sy (GPa)'].values[:, None]
+        elif self.yname.startswith('sigma_'):
+            e_plastic = self.yname[6:]
+            self.y = df['s' + e_plastic + ' (GPa)'].values[:, None]
 
 class ExpDataT(object):
     def __init__(self, temp, yname):
@@ -116,7 +162,7 @@ class BerkovichData(object):
 
         self.X = df[['C (GPa)', 'dP/dh (N/m)', 'Wp/Wt']].values
         if self.yname == 'Estar':
-            self.y = EtoEr(df['E (GPa)', 'nu'].values)[:, None]
+            self.y = EtoEr(df['E (GPa)'].values, df['nu'].values)[:, None]
         elif self.yname == 'sigma_y':
             self.y = df['sy (GPa)'].values[:, None]
         elif self.yname == 'n':
--- src/runmultiple.py
+++ src/runmultiple.py
@@ -8,7 +8,24 @@ def run_main(arg):
 if __name__ == '__main__':
 
     arguments = np.array([
-        "validation_one('Estar', ['TI33_750', 'TI33_500], 'TI33_750', 'Exp', [20])"
+        "validation_two('Estar', 1, 'TI33_25', 'FEM', 'Exp')",
+        "validation_two('Estar', 2, 'TI33_25', 'FEM', 'Exp')",
+        "validation_two('Estar', 3, 'TI33_25', 'FEM', 'Exp')",
+        "validation_two('Estar', 4, 'TI33_25', 'FEM', 'Exp')",
+        "validation_two('Estar', 5, 'TI33_25', 'FEM', 'Exp')",
+        "validation_two('Estar', 6, 'TI33_25', 'FEM', 'Exp')",
+        "validation_two('Estar', 8, 'TI33_25', 'FEM', 'Exp')",
+        "validation_two('Estar', 10, 'TI33_25', 'FEM', 'Exp')",
+        "validation_two('Estar', 20, 'TI33_25', 'FEM', 'Exp')",
+        "validation_two('sigma_y', 1, 'TI33_25', 'FEM', 'Exp')",
+        "validation_two('sigma_y', 2, 'TI33_25', 'FEM', 'Exp')",
+        "validation_two('sigma_y', 3, 'TI33_25', 'FEM', 'Exp')",
+        "validation_two('sigma_y', 4, 'TI33_25', 'FEM', 'Exp')",
+        "validation_two('sigma_y', 5, 'TI33_25', 'FEM', 'Exp')",
+        "validation_two('sigma_y', 6, 'TI33_25', 'FEM', 'Exp')",
+        "validation_two('sigma_y', 8, 'TI33_25', 'FEM', 'Exp')",
+        "validation_two('sigma_y', 10, 'TI33_25', 'FEM', 'Exp')",
+        "validation_two('sigma_y', 20, 'TI33_25', 'FEM', 'Exp')"
         ])
     
     processes = []
@@ -24,30 +41,42 @@ if __name__ == '__main__':
 
 
         '''
-        "validation_one('sigma_y', ['TI33_750a'], 'TI33_750a', 'Exp', [1])",
-        "validation_one('sigma_y', ['TI33_750a'], 'TI33_750a', 'Exp', [2])",
-        "validation_one('sigma_y', ['TI33_750a'], 'TI33_750a', 'Exp', [3])",
-        "validation_one('sigma_y', ['TI33_750a'], 'TI33_750a', 'Exp', [4])",
-        "validation_one('sigma_y', ['TI33_750a'], 'TI33_750a', 'Exp', [5])",
-        "validation_one('sigma_y', ['TI33_750a'], 'TI33_750a', 'Exp', [10])",
-        "validation_one('sigma_y', ['TI33_750a'], 'TI33_750a', 'Exp', [20])",
-        "validation_one('Estar', ['TI33_750a'], 'TI33_750a', 'Exp', [1])",
-        "validation_one('Estar', ['TI33_750a'], 'TI33_750a', 'Exp', [2])",
-        "validation_one('Estar', ['TI33_750a'], 'TI33_750a', 'Exp', [3])",
-        "validation_one('Estar', ['TI33_750a'], 'TI33_750a', 'Exp', [4])",
-        "validation_one('Estar', ['TI33_750a'], 'TI33_750a', 'Exp', [5])",
-        "validation_one('Estar', ['TI33_750a'], 'TI33_750a', 'Exp', [10])",'''
-        '''"validation_one('sigma_y', ['TI33_25a', 'TI33_250a', 'TI33_500a'], 'TI33_750a', 'Exp', [1, 1, 1])",
-        "validation_one('sigma_y', ['TI33_25a', 'TI33_250a', 'TI33_500a'], 'TI33_750a', 'Exp', [2, 2, 2])",
-        "validation_one('sigma_y', ['TI33_25a', 'TI33_250a', 'TI33_500a'], 'TI33_750a', 'Exp', [3, 3, 3])",
-        "validation_one('sigma_y', ['TI33_25a', 'TI33_250a', 'TI33_500a'], 'TI33_750a', 'Exp', [4, 4, 4])",
-        "validation_one('sigma_y', ['TI33_25a', 'TI33_250a', 'TI33_500a'], 'TI33_750a', 'Exp', [5, 5, 5])",
-        "validation_one('sigma_y', ['TI33_25a', 'TI33_250a', 'TI33_500a'], 'TI33_750a', 'Exp', [10, 10, 10])",
-        "validation_one('sigma_y', ['TI33_25a', 'TI33_250a', 'TI33_500a'], 'TI33_750a', 'Exp', [20, 20, 20])",
-        "validation_one('Estar', ['TI33_25a', 'TI33_250a', 'TI33_500a'], 'TI33_750a', 'Exp', [1, 1, 1])",
-        "validation_one('Estar', ['TI33_25a', 'TI33_250a', 'TI33_500a'], 'TI33_750a', 'Exp', [2, 2, 2])",
-        "validation_one('Estar', ['TI33_25a', 'TI33_250a', 'TI33_500a'], 'TI33_750a', 'Exp', [3, 3, 3])",
-        "validation_one('Estar', ['TI33_25a', 'TI33_250a', 'TI33_500a'], 'TI33_750a', 'Exp', [4, 4, 4])",
-        "validation_one('Estar', ['TI33_25a', 'TI33_250a', 'TI33_500a'], 'TI33_750a', 'Exp', [5, 5, 5])",
-        "validation_one('Estar', ['TI33_25a', 'TI33_250a', 'TI33_500a'], 'TI33_750a', 'Exp', [10, 10, 10])",
-        "validation_one('Estar', ['TI33_25a', 'TI33_250a', 'TI33_500a'], 'TI33_750a', 'Exp', [20, 20, 20])"'''
+        "validation_one('Estar', ['TI33_25'], 'TI33_25', 'Exp', [1])",
+        "validation_one('Estar', ['TI33_25'], 'TI33_25', 'Exp', [2])",
+        "validation_one('Estar', ['TI33_25'], 'TI33_25', 'Exp', [3])",
+        "validation_one('Estar', ['TI33_25'], 'TI33_25', 'Exp', [4])",
+        "validation_one('Estar', ['TI33_25'], 'TI33_25', 'Exp', [5])",
+        "validation_one('Estar', ['TI33_25'], 'TI33_25', 'Exp', [6])",
+        "validation_one('Estar', ['TI33_25'], 'TI33_25', 'Exp', [8])",
+        "validation_one('Estar', ['TI33_25'], 'TI33_25', 'Exp', [10])",
+        "validation_one('Estar', ['TI33_25'], 'TI33_25', 'Exp', [20])",
+        "validation_one('sigma_y', ['TI33_25'], 'TI33_25', 'Exp', [1])",
+        "validation_one('sigma_y', ['TI33_25'], 'TI33_25', 'Exp', [2])",
+        "validation_one('sigma_y', ['TI33_25'], 'TI33_25', 'Exp', [3])",
+        "validation_one('sigma_y', ['TI33_25'], 'TI33_25', 'Exp', [4])",
+        "validation_one('sigma_y', ['TI33_25'], 'TI33_25', 'Exp', [5])",
+        "validation_one('sigma_y', ['TI33_25'], 'TI33_25', 'Exp', [6])",
+        "validation_one('sigma_y', ['TI33_25'], 'TI33_25', 'Exp', [8])",
+        "validation_one('sigma_y', ['TI33_25'], 'TI33_25', 'Exp', [10])",
+        "validation_one('sigma_y', ['TI33_25'], 'TI33_25', 'Exp', [20])"
+        '''
+        '''
+        "validation_two('Estar', 1, 'TI33_25', 'Berk', 'Exp')",
+        "validation_two('Estar', 2, 'TI33_25', 'Berk', 'Exp')",
+        "validation_two('Estar', 3, 'TI33_25', 'Berk', 'Exp')",
+        "validation_two('Estar', 4, 'TI33_25', 'Berk', 'Exp')",
+        "validation_two('Estar', 5, 'TI33_25', 'Berk', 'Exp')",
+        "validation_two('Estar', 6, 'TI33_25', 'Berk', 'Exp')",
+        "validation_two('Estar', 8, 'TI33_25', 'Berk', 'Exp')",
+        "validation_two('Estar', 10, 'TI33_25', 'Berk', 'Exp')",
+        "validation_two('Estar', 20, 'TI33_25', 'Berk', 'Exp')",
+        "validation_two('sigma_y', 1, 'TI33_25', 'Berk', 'Exp')",
+        "validation_two('sigma_y', 2, 'TI33_25', 'Berk', 'Exp')",
+        "validation_two('sigma_y', 3, 'TI33_25', 'Berk', 'Exp')",
+        "validation_two('sigma_y', 4, 'TI33_25', 'Berk', 'Exp')",
+        "validation_two('sigma_y', 5, 'TI33_25', 'Berk', 'Exp')",
+        "validation_two('sigma_y', 6, 'TI33_25', 'Berk', 'Exp')",
+        "validation_two('sigma_y', 8, 'TI33_25', 'Berk', 'Exp')",
+        "validation_two('sigma_y', 10, 'TI33_25', 'Berk', 'Exp')",
+        "validation_two('sigma_y', 20, 'TI33_25', 'Berk', 'Exp')"
+        '''
