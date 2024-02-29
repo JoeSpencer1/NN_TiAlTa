9b45df9c2e405ec600bfa34368b18d840a3a99c4
JoeSpencer1 <108445563+JoeSpencer1@users.noreply.github.com>
2024-02-29 15:00:23 -0700

    Convergence study isn't quite working for 2D case. I think this is because there are some bumps in the mesh.

--- src/convergence.py
+++ src/convergence.py
@@ -11,7 +11,7 @@ x3, y3, z3 = df['X3 (m)'], df['Y3 (m)'], df['Z3 (m)']
 D1, D2, D3 = df['D1 (m)'], df['D2 (m)'], df['D3 (m)']
 df['r2'], df['r3'] = np.full((len(df['X1 (m)']),), 100), np.full((len(df['X1 (m)']),), 100)
 df['err2'], df['err3'] = df['X1 (m)'], df['X1 (m)']
-
+'''
 for i in range(len(x1)):
     if np.isnan(D1[i]):
         break
@@ -51,7 +51,7 @@ print('e2=', e2)
 print('e3=', e3)
 print('p=', np.log(e1/e3)/np.log(4))
 print('C=', e1 / maxh ** (np.log(e1/e3)/np.log(4)))
-
+'''
 # Quadratic case
 df = pd.read_excel('../data/conv/3D_qua.xlsx')
 print(df)
@@ -102,7 +102,7 @@ print('e2=', e2)
 print('e3=', e3)
 print('p=', np.log(e1/e3)/np.log(4))
 print('C=', e1 / maxh ** (np.log(e1/e3)/np.log(4)))
-
+'''
 # 30˚ case
 df = pd.read_excel('../data/conv/2D_30.xlsx')
 print(df)
@@ -255,7 +255,7 @@ print('e2=', e2)
 print('e3=', e3)
 print('p=', np.log(e1/e3)/np.log(4))
 print('C=', e1 / maxh ** (np.log(e1/e3)/np.log(4)))
-
+'''
 # 70˚ case
 df = pd.read_excel('../data/conv/2D_70.xlsx')
 print(df)
@@ -300,7 +300,7 @@ for i in range(len(x1)):
 e1 = np.sqrt(e1 / leng)
 e2 = np.sqrt(e2 / leng)
 e3 = np.sqrt(e3 / leng)
-maxh = 0.1
+maxh = 0.5
 print('\ne1=', e1)
 print('e2=', e2)
 print('e3=', e3)
