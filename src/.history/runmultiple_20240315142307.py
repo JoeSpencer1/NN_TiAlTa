5863db5a3c7fb66108c0ff9539443e0bfd591e31
JoeSpencer1 <108445563+JoeSpencer1@users.noreply.github.com>
2024-03-15 14:50:04 -0600

    Commit before pulling new data for convergence study.

---
+++ src/.history/runmultiple_20240315142307.py
@@ -0,0 +1,38 @@
+import multiprocessing
+from multiprocessing import Pool
+import numpy as np
+import nn
+import os
+
+def run_main(arg):
+    nn.main(arg)
+
+
+if __name__ == '__main__':
+
+    arguments = np.array([
+        "validation_two('Er', 'TI33_25', 'TI33_25', 10, '3D_quad', 10, v_lo=0)",
+        "validation_two('sigma_y', 'TI33_25', 'TI33_25', 10, '3D_quad', 10, v_lo=0)",
+        "validation_two('Er', 'TI33_25', 'TI33_25', 10, '3D_quad', 10, v_lo=0.3)",
+        "validation_two('sigma_y', 'TI33_25', 'TI33_25', 10, '3D_quad', 10, v_lo=0.3)"
+        ])
+    
+    processes = []
+    num_processes = len(arguments)
+    for i in range(num_processes):        
+        process = multiprocessing.Process(target=run_main, args=(arguments[i],))
+        processes.append(process)
+
+    for process in processes:
+        process.start()
+    for process in processes:
+        process.join()
+    with open('output.txt', 'a') as f:
+        f.write('\n')
+
+        '''
+        "validation_three('Er', 'Lu et al/B3090', 'Lu et al/B3090', 100, 'Lu et al/Berkovich', 14, 'Lu et al/FEM_70deg', 100, typ='hi')",
+        "validation_three('sigma_y', 'Lu et al/B3090', 'Lu et al/B3090', 100, 'Lu et al/Berkovich', 14, 'Lu et al/FEM_70deg', 100, typ='hi')",
+        "validation_three('Er', 'Lu et al/B3090', 'Lu et al/B3090', 100, 'Lu et al/Berkovich', 14, 'Lu et al/FEM_70deg', 100, typ='lo')",
+        "validation_three('sigma_y', 'Lu et al/B3090', 'Lu et al/B3090', 100, 'Lu et al/Berkovich', 14, 'Lu et al/FEM_70deg', 100, typ='lo')"
+        '''
